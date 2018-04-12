c5986e1d9654a3a1b59802c50b2739728f3d3349
sqrtthree <imsqrtthree@gmail.com>
2018-04-12 23:30:42 +0800

    :bento: chore(examples): Improve the output of example file

--- _examples/main.go
+++ _examples/main.go
@@ -7,7 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Println(trending.Today().Repos())
+	repos, _ := trending.Today().Repos()
+
+	for index, repo := range repos {
+		fmt.Println(index + 1, repo.Title)
+	}
+
 	// fmt.Println(trending.ThisWeek().Repos())
 	// fmt.Println(trending.ThisMonth().Repos())
 	// fmt.Println(trending.Repos())
